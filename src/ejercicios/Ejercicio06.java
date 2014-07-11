4d287f03293e1dc379671ddeb42e0c16f6532ecf
Melissa Mendoza <princesa_1.9.9.2@hotmail.com>
2014-07-11 10:36:53 -0500

    commit

---
+++ src/ejercicios/Ejercicio06.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejercicios;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Eugenio
+ */
+public class Ejercicio06 
+{
+    public static void main(String[] args)
+    {
+        String a="";
+        String palabra=JOptionPane.showInputDialog(null,"Ingrese uns frase : ");
+        String []frase=palabra.split(" ");
+        for(int i=frase.length-1;i>=0;i--)
+        {
+            a=a+frase[i]+" ";            
+        }
+        JOptionPane.showMessageDialog(null, a);
+    }
+}
